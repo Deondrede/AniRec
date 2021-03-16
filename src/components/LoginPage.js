2e4e4182a90fcae2f86b1275e075b32fab6f508d
knprogram <Kevin.Ng022@myhunter.cuny.edu>
2021-03-16 13:10:18 -0400

    updating loginpage

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -8,32 +8,34 @@ import SignUp from "./signup.component";
 function LoginPage(){
 
     return(
-        <div className="login">
-            <h1>AniRec</h1>
-            <p>A plot based Anime Recommender</p>
-            <p>made by students of CUNY Hunter College Spring 2021 CSCI 499</p>
+        <div className="logbg">
+            <div className="login">
+                <h1>AniRec</h1>
+                <p>A plot based Anime Recommender</p>
+                <p>made by students of CUNY Hunter College Spring 2021 CSCI 499</p>
 
-            <Router>
-            <div className="App">
-                
-                <button>
-                    <Link to={"/log-in"}>Log in</Link>
-                </button>
-                <button>
-                    <Link to={"/register"}>Register</Link>
-                </button>
-        
-            <div className="outer">
-                <div className="inner">
-                <Switch>
-                    <Route exact path='/' component={Login} />
-                    <Route path="/log-in" component={Login} />
-                    <Route path="/register" component={SignUp} />
-                </Switch>
+                <Router>
+                <div className="App">
+                    
+                    <button>
+                        <Link to={"/log-in"}>Log in</Link>
+                    </button>
+                    <button>
+                        <Link to={"/register"}>Register</Link>
+                    </button>
+            
+                <div className="outer">
+                    <div className="inner">
+                    <Switch>
+                        <Route exact path='/' component={Login} />
+                        <Route path="/log-in" component={Login} />
+                        <Route path="/register" component={SignUp} />
+                    </Switch>
+                    </div>
                 </div>
-            </div>
-            </div></Router>
+                </div></Router>
 
+            </div>
         </div>
     );
 }
