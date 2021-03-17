6d3c7bef2254831685dd1eb12e837b6a854769f6
Jason Chen <Jason.Chen53@myhunter.cuny.edu>
2021-03-16 21:52:08 -0400

    updated with bootstrap

--- test/src/components/LoginPage.js
+++ test/src/components/LoginPage.js
@@ -23,8 +23,6 @@ function LoginPage(){
                 <Row className="login">
                     <Col>
                         <h1>AniRec</h1>
-                        <p>A plot based Anime Recommender</p>
-                        <p>made by students of CUNY Hunter College Spring 2021 CSCI 499</p>
 
                         <Router>
                             <div className="App">                
