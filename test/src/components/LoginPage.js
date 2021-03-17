353f2cc3605a6625257815b46b5cb32ca0efdec1
Jason Chen <Jason.Chen53@myhunter.cuny.edu>
2021-03-16 21:51:25 -0400

    updated with bootstrap

--- test/src/components/LoginPage.js
+++ test/src/components/LoginPage.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Container from "react-bootstrap";
 import "./Loginpage.css";
+import { Container, Row, Col } from "react-bootstrap"
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Login from "./login.component";
@@ -9,34 +9,41 @@ import SignUp from "./signup.component";
 function LoginPage(){
 
     return(
-            <Container className="logbg">
-                <div className="login">
-                    <h1>AniRec</h1>
-                    <p>A plot based Anime Recommender</p>
-                    <p>made by students of CUNY Hunter College Spring 2021 CSCI 499</p>
-
-                    <Router>
-                    <div className="App">
-                        
+            <Container fluid>
+                <Row className="login">
+                    <Col>
                         <button>
                             <Link to={"/log-in"}>Log in</Link>
                         </button>
                         <button>
                             <Link to={"/register"}>Register</Link>
-                        </button>
-                
-                    <div className="outer">
-                        <div className="inner">
-                        <Switch>
-                            <Route exact path='/' component={Login} />
-                            <Route path="/log-in" component={Login} />
-                            <Route path="/register" component={SignUp} />
-                        </Switch>
-                        </div>
-                    </div>
-                    </div></Router>
+                        </button>                    
+                    </Col>
+                </Row>
+                <Row className="login">
+                    <Col>
+                        <h1>AniRec</h1>
+                        <p>A plot based Anime Recommender</p>
+                        <p>made by students of CUNY Hunter College Spring 2021 CSCI 499</p>
 
-                </div>
+                        <Router>
+                            <div className="App">                
+                                <div className="outer">
+                                    <div className="inner">
+                                        <Switch>
+                                            <Route exact path='/' component={Login} />
+                                            <Route path="/log-in" component={Login} />
+                                            <Route path="/register" component={SignUp} />
+                                        </Switch>
+                                    </div>
+                                </div>
+                            </div>
+                        </Router>
+                    </Col>                    
+                </Row>
+                <Row className="test">
+                    <p>Testing block</p>
+                </Row>
             </Container>
     );
 }
