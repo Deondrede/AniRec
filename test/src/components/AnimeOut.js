8ab96d82ad988fa85fe21008bdf200c8aa63c00d
Jason Chen <Jason.Chen53@myhunter.cuny.edu>
2021-03-18 19:24:55 -0400

    added navbar

--- test/src/components/AnimeOut.js
+++ test/src/components/AnimeOut.js
@@ -1,80 +1,85 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ShowAnime from './ShowAnime.js';
 
 import { Container, Row, Col, Button } from "react-bootstrap";
+import Header from "./Header"
 
 //will be used as next page to list
 function AnimeOut(){
     return(
-        <Container>
-        <div>
-            <div className="anime-search">
-                <form className="search-form">
-                    <input className="search-bar" type="text" />
-                    <button className="search-button" type="submit">
-                        Search
-                    </button>
-                </form>
+        <Fragment>
+            <Header />
+            <Container>
+            <div>
+                <div className="anime-search">
+                    <form className="search-form">
+                        <input className="search-bar" type="text" />
+                        <button className="search-button" type="submit">
+                            Search
+                        </button>
+                    </form>
+                </div>
+                <Row id="feed_body">
+                    <Col id="feed_col">
+                        <span><strong>Recommended</strong></span>
+                        <ShowAnime 
+                            name="Test Anime" 
+                            image="[image]" 
+                            genre= "action, adventure, tragedy, drama" 
+                            studio="New Studio"/>
+                        <ShowAnime 
+                            name="Test Anime 2" 
+                            image="[image]" 
+                            genre= "romance, slice of life, comedy, shoujo" 
+                            studio="Veteran Studio"/>
+                    </Col>
+                    <Col id="spacing"></Col>
+                    <Col id="feed_col">
+                        <span><strong>Now Airing</strong></span>
+                        <ShowAnime 
+                            name="Test Anime" 
+                            image="[image]" 
+                            genre= "action, adventure, tragedy, drama" 
+                            studio="New Studio"/>
+                        <ShowAnime 
+                            name="Test Anime 2" 
+                            image="[image]" 
+                            genre= "romance, slice of life, comedy, shoujo" 
+                            studio="Veteran Studio"/>
+                    </Col>
+                    <Col id="spacing"></Col>
+                    <Col id="feed_col">
+                        <span><strong>Try This</strong></span>
+                        <ShowAnime 
+                            name="Test Anime" 
+                            image="[image]" 
+                            genre= "action, adventure, tragedy, drama" 
+                            studio="New Studio"/>
+                        <ShowAnime 
+                            name="Test Anime 2" 
+                            image="[image]" 
+                            genre= "romance, slice of life, comedy, shoujo" 
+                            studio="Veteran Studio"/>
+                    </Col>
+                    <Col id="spacing"></Col>
+                    <Col id="feed_col">
+                        <span><strong>Trending</strong></span>
+                        <ShowAnime 
+                            name="Test Anime" 
+                            image="[image]" 
+                            genre= "action, adventure, tragedy, drama" 
+                            studio="New Studio"/>
+                        <ShowAnime 
+                            name="Test Anime 2" 
+                            image="[image]" 
+                            genre= "romance, slice of life, comedy, shoujo" 
+                            studio="Veteran Studio"/>
+                    </Col>
+                </Row>
             </div>
-            <Row id="feed_body">
-                <Col id="feed_col">
-                    <span><strong>Recommended</strong></span>
-                    <ShowAnime 
-                        name="Test Anime" 
-                        image="[image]" 
-                        genre= "action, adventure, tragedy, drama" 
-                        studio="New Studio"/>
-                    <ShowAnime 
-                        name="Test Anime 2" 
-                        image="[image]" 
-                        genre= "romance, slice of life, comedy, shoujo" 
-                        studio="Veteran Studio"/>
-                </Col>
-                <Col id="spacing"></Col>
-                <Col id="feed_col">
-                    <span><strong>Now Airing</strong></span>
-                    <ShowAnime 
-                        name="Test Anime" 
-                        image="[image]" 
-                        genre= "action, adventure, tragedy, drama" 
-                        studio="New Studio"/>
-                    <ShowAnime 
-                        name="Test Anime 2" 
-                        image="[image]" 
-                        genre= "romance, slice of life, comedy, shoujo" 
-                        studio="Veteran Studio"/>
-                </Col>
-                <Col id="spacing"></Col>
-                <Col id="feed_col">
-                    <span><strong>Try This</strong></span>
-                    <ShowAnime 
-                        name="Test Anime" 
-                        image="[image]" 
-                        genre= "action, adventure, tragedy, drama" 
-                        studio="New Studio"/>
-                    <ShowAnime 
-                        name="Test Anime 2" 
-                        image="[image]" 
-                        genre= "romance, slice of life, comedy, shoujo" 
-                        studio="Veteran Studio"/>
-                </Col>
-                <Col id="spacing"></Col>
-                <Col id="feed_col">
-                    <span><strong>Trending</strong></span>
-                    <ShowAnime 
-                        name="Test Anime" 
-                        image="[image]" 
-                        genre= "action, adventure, tragedy, drama" 
-                        studio="New Studio"/>
-                    <ShowAnime 
-                        name="Test Anime 2" 
-                        image="[image]" 
-                        genre= "romance, slice of life, comedy, shoujo" 
-                        studio="Veteran Studio"/>
-                </Col>
-            </Row>
-        </div>
-        </Container>
+            </Container>
+        </Fragment>
+
     );
 }
 
--- test/src/components/LoginPage.js
+++ test/src/components/LoginPage.js
@@ -1,48 +1,51 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./Loginpage.css";
 
 import Login from "./login.component";
 import SignUp from "./signup.component";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from "./Header"
 
 //function for login page = home page
 function LoginPage(){
 
     return(
+        <Fragment>
+            <Header />
             <Container fluid>
-                <Row id="tabs">
-                    <Col>
-                        <Button id="sign_in" variant="flat">
-                            <Link to={"/log-in"}>Log in</Link>
-                        </Button>
-                        <Button  id="register" variant="flat">
-                            <Link to={"/register"}>Register</Link>
-                        </Button>                    
-                    </Col>
-                </Row>
-                <Row className="login">
-                    <Col>
-                        <h1>AniRec</h1>
+            <Row id="tabs">
+                <Col>
+                    <Button id="sign_in" variant="flat">
+                        <Link id="tab_name" to={"/log-in"}>Log in</Link>
+                    </Button>
+                    <Button  id="register" variant="flat">
+                        <Link id="tab_name" to={"/register"}>Register</Link>
+                    </Button>                    
+                </Col>
+            </Row>
+            <Row id="login">
+                <Col>
+                    <h1>AniRec</h1>
 
-                        <Router>
-                            <div className="App">                
-                                <div className="outer">
-                                    <div className="inner">
-                                        <Switch>
-                                            <Route exact path='/' component={Login} />
-                                            <Route path="/log-in" component={Login} />
-                                            <Route path="/register" component={SignUp} />
-                                        </Switch>
-                                    </div>
+                    <Router>
+                        <div className="App">                
+                            <div className="outer">
+                                <div className="inner">
+                                    <Switch>
+                                        <Route exact path='/' component={Login} />
+                                        <Route path="/log-in" component={Login} />
+                                        <Route path="/register" component={SignUp} />
+                                    </Switch>
                                 </div>
                             </div>
-                        </Router>
-                    </Col>                    
-                </Row>
-
-
-            </Container>
+                        </div>
+                    </Router>
+                </Col>                    
+            </Row>
+        </Container>
+        </Fragment>
+  
     );
 }
 
