cd80d725cb6f2eea5f00cc3e22c197eee7799bb8
Deondre De Freitas <deondrede@gmail.com>
2021-03-23 22:17:09 -0400

    Some more instructions for the front end

--- anirec/src/components/landing_page/login.component.js
+++ anirec/src/components/landing_page/login.component.js
@@ -1,10 +1,29 @@
 import React, { Component } from "react";
-// I use axios for this but if you want to try using something else like ajax you're free to do so
+// I used axios for this but if you want to try using something else like ajax you're free to do so
 // Also axios is a separate module so don't forget to do 'npm install axios'
 const axios = require('axios').default;
 
 
-//Probably don't need to set these CSRF tokens, too lazy to remove to see what ungodly errors might appear
+// An example of a GET request
+axios.get('http://localhost:5000').then((response) =>{
+        console.log(response.data);
+});
+
+// How the login is going to work
+// Note: This isn't the most secure way to do this but since the crux of this project isn't in the security then it should be fine
+// Setting the cookie is going to be how we're going to keep track of what user is signed in
+// If they log out then we either reset the cookie to "" or delete it entirely and redirect back to login page
+// function onLogin(){
+    // Compare what's in the username and password text fields with what's in the database via a GET request
+    // If they match then we can set the cookie to the username of the logged in user and redirect them to the main page
+    // document.cookie = "username=jsondata.fields.username; path/"; The "/" means that it will be set for all pages
+// } We can then use the cookie to determine what user info we need to load e.g preferred genres
+
+
+
+
+
+//Probably don't need to set these CSRF tokens, but too lazy to remove to see what ungodly errors might appear
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.withCredentials = true;
@@ -13,7 +32,7 @@ axios.defaults.withCredentials = true;
 // Since this is the login page and not the register page, once the register page is done we can just copy this over
 // Implemented it here just for testing purposes
 function register(){
-    // All requests need to be sent int the FormData format
+    // All requests need to be sent in the FormData format
     let bodyFormData = new FormData();
     // Obtain the values from the textfields and append them to the FormData
     let username = document.getElementById("username").value;
@@ -21,7 +40,7 @@ function register(){
     bodyFormData.append("username", username);
     bodyFormData.append("password", password);
     // Sends the POST request to the backend, all POST requests are sent to the localhost:5000/users
-    // Sending requests to the normal localhost:5000 will not fulfil any requests
+    // Sending POST requests to the normal localhost:5000 will not fulfil any requests
     axios({
         method: "post",
         url: "http://localhost:5000/users",
