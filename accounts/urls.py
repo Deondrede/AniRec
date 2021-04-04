d1bea924e0ee7f37dd1e7cd793d231842a93524f
Deondre De Freitas <deondrede@gmail.com>
2021-04-04 17:32:13 -0400

    added support for updating user preferences

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,7 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('users', views.createUser, name="create")
+    path('users', views.createUser, name="users"),
+    path('genres', views.updateGenre, name="genre"),
+    path('anime', views.updateAnime, name="anime")
 ]
--- anirec/src/components/landing_page/login.component.js
+++ anirec/src/components/landing_page/login.component.js
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 // Also axios is a separate module so don't forget to do 'npm install axios'
 const axios = require('axios').default;
 
-
 // An example of a GET request
 axios.get('http://localhost:5000').then((response) =>{
         console.log(response.data);
@@ -11,16 +10,28 @@ axios.get('http://localhost:5000').then((response) =>{
 
 // How the login is going to work
 // Note: This isn't the most secure way to do this but since the crux of this project isn't in the security then it should be fine
-// Setting the cookie is going to be how we're going to keep track of what user is signed in
-// If they log out then we either reset the cookie to "" or delete it entirely and redirect back to login page
-// function onLogin(){
-    // Compare what's in the username and password text fields with what's in the database via a GET request
-    // If they match then we can set the cookie to the username of the logged in user and redirect them to the main page
-    // document.cookie = "username=jsondata.fields.username; path/"; The "/" means that it will be set for all pages
-// } We can then use the cookie to determine what user info we need to load e.g preferred genres
-
-
-
+// We're going to set check whether a user is logged by making use of local storage,
+// on login, a key value with their username is going to be set, we're going to use that value to load their prefernces,
+// and once they log out we can delete what's inside local storage and return them to the main page
+function onLogin(){
+    axios.get('http://localhost:5000').then((response) =>{
+        let getRequest = response.data
+        let username = document.getElementById("username").value;
+        let password = document.getElementById("password").value;
+        for (let user in getRequest){
+            if (getRequest[user]["fields"]["username"] == username && getRequest[user]["fields"]["password"] == password){
+                window.localStorage.setItem("username", username); // Set the user's username in local storage
+                //Transition to next screen, however this is supposed to work in React
+            }
+            else{
+                alert("No user found")
+            }
+        }
+    });
+}
+// Compare what's in the username and password text fields with what's in the database via a GET request
+// If they match then we can set the cookie to the username of the logged in user and redirect them to the main page
+// We can then use the cookie to determine what user info we need to load e.g preferred genres
 
 
 //Probably don't need to set these CSRF tokens, but too lazy to remove to see what ungodly errors might appear
@@ -74,7 +85,7 @@ export default class Login extends Component {
                         <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
                     </div>
                 </div>
-                <button type="submit" className="btn btn-dark btn-lg btn-block" onClick={register}>Sign in</button>
+                <button type="submit" className="btn btn-dark btn-lg btn-block" onClick={onLogin}>Sign in</button>
             </form>
         );
     }
