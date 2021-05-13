5f01c5161f2f222500b17e63b5b6a26d2b5a122e
Kevin Ng <37814555+knprogram@users.noreply.github.com>
2021-05-13 19:25:47 -0400

    individual pages

--- test/anirec/src/pages/home_page/Homepage.js
+++ test/anirec/src/pages/home_page/Homepage.js
@@ -57,79 +57,86 @@ function HomePage(){
 
     if (errorC3) return <p>Error3 :(</p>
 
-    let col1_1 = {
-        name: (dataC2.series1.title.english==null)
-            ? dataC2.series1.title.romaji :
-            dataC2.series1.title.english,
-        image: dataC2.series1.coverImage.extraLarge,
-        genre: dataC2.series1.genres,
-        studio: dataC2.series1.studios.nodes[0].name
-    }
-
+        let col1_1 = {
+            name: (dataC2.series1.title.english==null)
+                ? dataC2.series1.title.romaji :
+                dataC2.series1.title.english,
+            image: dataC2.series1.coverImage.extraLarge,
+            genre: dataC2.series1.genres,
+            studio: dataC2.series1.studios.nodes[0].name,
+            id: dataC2.series1.id
+        }
     
-    let col1_2 = {
-        name: (dataC2.series2.title.english==null)
-            ? dataC2.series2.title.romaji :
-            dataC2.series2.title.english,
-        image: dataC2.series2.coverImage.extraLarge,
-        genre: dataC2.series2.genres,
-        studio: dataC2.series2.studios.nodes[0].name
-    }
+        
+        let col1_2 = {
+            name: (dataC2.series2.title.english==null)
+                ? dataC2.series2.title.romaji :
+                dataC2.series2.title.english,
+            image: dataC2.series2.coverImage.extraLarge,
+            genre: dataC2.series2.genres,
+            studio: dataC2.series2.studios.nodes[0].name,
+            id: dataC2.series2.id
+        }
+        
+        let col1_3= {
+            name: (dataC2.series3.title.english==null)
+                ? dataC2.series3.title.romaji :
+                dataC2.series3.title.english,
+            image: dataC2.series3.coverImage.extraLarge,
+            genre: dataC2.series3.genres,
+            studio: dataC2.series3.studios.nodes[0].name,
+            id: dataC2.series3.id
+        }
     
-    let col1_3= {
-        name: (dataC2.series3.title.english==null)
-            ? dataC2.series3.title.romaji :
-            dataC2.series3.title.english,
-        image: dataC2.series3.coverImage.extraLarge,
-        genre: dataC2.series3.genres,
-        studio: dataC2.series3.studios.nodes[0].name
-    }
-
-    let col1_4 = {
-        name: (dataC2.series4.title.english==null)
-        ? dataC2.series4.title.romaji :
-        dataC2.series4.title.english,
-        image: dataC2.series4.coverImage.extraLarge,
-        genre: dataC2.series4.genres,
-        studio: dataC2.series4.studios.nodes[0].name
-    }
-
-    let col3_1 = {
-        name: (dataC3.series1.title.english==null)
-            ? dataC3.series1.title.romaji :
-            dataC3.series1.title.english,
-        image: dataC3.series1.coverImage.extraLarge,
-        genre: dataC3.series1.genres,
-        studio: dataC3.series1.studios.nodes[0].name
-    }
-
-    let col3_2 = {
-        name: (dataC3.series2.title.english==null)
-            ? dataC3.series2.title.romaji :
-            dataC3.series2.title.english,
-        image: dataC3.series2.coverImage.extraLarge,
-        genre: dataC3.series2.genres,
-        studio: dataC3.series2.studios.nodes[0].name
-    }
+        let col1_4 = {
+            name: (dataC2.series4.title.english==null)
+            ? dataC2.series4.title.romaji :
+            dataC2.series4.title.english,
+            image: dataC2.series4.coverImage.extraLarge,
+            genre: dataC2.series4.genres,
+            studio: dataC2.series4.studios.nodes[0].name,
+            id: dataC2.series4.id
+        }
     
-    let col3_3= {
-        name: (dataC3.series3.title.english==null)
-            ? dataC3.series3.title.romaji :
-            dataC3.series3.title.english,
-        image: dataC3.series3.coverImage.extraLarge,
-        genre: dataC3.series3.genres,
-        studio: dataC3.series3.studios.nodes[0].name
-    }
-
-    let col3_4 = {
-        name: (dataC3.series4.title.english==null)
-        ? dataC3.series4.title.romaji :
-        dataC3.series4.title.english,
-        image: dataC3.series4.coverImage.extraLarge,
-        genre: dataC3.series4.genres,
-        studio: dataC3.series4.studios.nodes[0].name
-    }
-  
+        let col3_1 = {
+            name: (dataC3.series1.title.english==null)
+                ? dataC3.series1.title.romaji :
+                dataC3.series1.title.english,
+            image: dataC3.series1.coverImage.extraLarge,
+            genre: dataC3.series1.genres,
+            studio: dataC3.series1.studios.nodes[0].name,
+            id: dataC3.series1.id
+        }
+    
+        let col3_2 = {
+            name: (dataC3.series2.title.english==null)
+                ? dataC3.series2.title.romaji :
+                dataC3.series2.title.english,
+            image: dataC3.series2.coverImage.extraLarge,
+            genre: dataC3.series2.genres,
+            studio: dataC3.series2.studios.nodes[0].name,
+            id: dataC3.series2.id
+        }
+        
+        let col3_3= {
+            name: (dataC3.series3.title.english==null)
+                ? dataC3.series3.title.romaji :
+                dataC3.series3.title.english,
+            image: dataC3.series3.coverImage.extraLarge,
+            genre: dataC3.series3.genres,
+            studio: dataC3.series3.studios.nodes[0].name,
+            id: dataC3.series3.id
+        }
+    
+        let col3_4 = {
+            name: (dataC3.series4.title.english==null)
+            ? dataC3.series4.title.romaji :
+            dataC3.series4.title.english,
+            image: dataC3.series4.coverImage.extraLarge,
+            genre: dataC3.series4.genres,
+            studio: dataC3.series4.studios.nodes[0].name,
+            id: dataC3.series4.id
+        }
     return(
         <Fragment>
             <Container>
@@ -143,26 +150,34 @@ function HomePage(){
                             <Col id="grey"><span></span></Col>
                         </Row>
                         <Row id="main_content">
-                            <HomePageShowCard
+                        <HomePageShowCard
                                 name={col1_1.name}
                                 image={col1_1.image}
                                 genre= {col1_1.genre}
-                                studio={col1_1.studio}/>
+                                studio={col1_1.studio}
+                                id={col1_1.id}
+                                />
                             <HomePageShowCard
                                 name={col1_2.name}
                                 image={col1_2.image}
                                 genre= {col1_2.genre}
-                                studio={col1_2.studio}/>
+                                studio={col1_2.studio}
+                                id={col1_2.id}
+                                />
                             <HomePageShowCard
                                 name={col1_3.name}
                                 image={col1_3.image}
                                 genre= {col1_3.genre}
-                                studio={col1_3.studio}/>
+                                studio={col1_3.studio}
+                                id={col1_3.id}
+                                />
                             <HomePageShowCard
                                 name={col1_4.name}
                                 image={col1_4.image}
                                 genre= {col1_4.genre}
-                                studio={col1_4.studio}/>
+                                studio={col1_4.studio}
+                                id={col1_4.id}
+                                />
 
                         </Row>
                     </Col>
@@ -190,7 +205,8 @@ function HomePage(){
                                         image={row.coverImage.extraLarge}
                                         genre={row.genres}
                                         studio={row.studios.nodes[0].name}
-                                        description={row.description}/>
+                                        description={row.description}
+                                        id={row.id}/>
                             )}
                         </Row>
                     </Col>
@@ -203,26 +219,34 @@ function HomePage(){
                             <Col id="grey"><span></span></Col>
                         </Row>
                         <Row id="main_content">
-                            <HomePageShowCard
+                        <HomePageShowCard
                                     name={col3_1.name}
                                     image={col3_1.image}
                                     genre={col3_1.genre}
-                                    studio={col3_1.studio}/>
+                                    studio={col3_1.studio}
+                                    id={col3_1.id}
+                                    />
                                 <HomePageShowCard
                                     name={col3_2.name}
                                     image={col3_2.image}
                                     genre={col3_2.genre}
-                                    studio={col3_2.studio}/>
+                                    studio={col3_2.studio}
+                                    id={col3_2.id}
+                                    />
                                 <HomePageShowCard
                                     name={col3_3.name}
                                     image={col3_3.image}
                                     genre={col3_3.genre}
-                                    studio={col3_3.studio}/>
+                                    studio={col3_3.studio}
+                                    id={col3_3.id}
+                                    />
                                 <HomePageShowCard
                                     name={col3_4.name}
                                     image={col3_4.image}
                                     genre={col3_4.genre}
-                                    studio={col3_4.studio}/>
+                                    studio={col3_4.studio}
+                                    id={col3_4.id}
+                                    />
                         </Row>
                     </Col>
                     <Col id="spacing"></Col>
@@ -249,7 +273,8 @@ function HomePage(){
                                     image={row.coverImage.extraLarge}
                                     genre={row.genres}
                                     studio={(row.studios.nodes[0]==null) ? "" : row.studios.nodes[0].name}
-                                    description={row.description}/>
+                                    description={row.description}
+                                    id={row.id}/>
                             )}
                         </Row>
                     </Col>
