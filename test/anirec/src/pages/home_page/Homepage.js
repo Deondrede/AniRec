0b69d495c1404125dd8206580dddc5c086e762c3
csangcap <csangcap405@gmail.com>
2021-05-16 18:58:37 -0400

    updated to pull Recommended anime
    It only works one way (Survey --> homepage). Going back another page and then back to home page, results in an error.

--- test/anirec/src/pages/home_page/Homepage.js
+++ test/anirec/src/pages/home_page/Homepage.js
@@ -2,105 +2,142 @@ import React, { Fragment, useEffect  } from "react";
 import HomePageShowCard from '../../components/HomePageShowCard';
 import TopSpace from "../../components/global_elements/TopSpacer"
 import {AIRING_NOW, TRY_THIS, TRENDING} from '../../GraphQL/Queries'
-import { Link } from "react-router-dom";
-import GetUserRecs from "../home_page/UserRecPulling.js";
+import { Link, useLocation } from "react-router-dom";
+//import GetUserRecs from "../home_page/UserRecPulling.js";
 
-import {useQuery} from '@apollo/client'
+import GetShow from "./GetRecShows.js";
+
+import {useQuery, gql} from '@apollo/client'
 import './Homepage.css';
 
 import { Container, Row, Col } from "react-bootstrap";
 //import ShowMore from "../listing_pages/ShowMore.js";
 
+// copied function from GetRecShows to this file
+function GetShow2(id_num) {
+    const REC_QUERY = gql`
+    query RecQuery($id: Int) 
+    {
+        Media(type: ANIME, id: $id)
+        {
+            id
+            title{
+                english
+                romaji
+            }
+            genres
+            description
+            coverImage{
+                medium
+                large
+                extraLarge
+            }
+            studios{
+                nodes{
+                    name
+                }
+            }
+        }
+    }`;
+    
+    //console.log("props.id", props.id);
+    const {error, loading, data} = useQuery(REC_QUERY,
+        {
+            variables: {
+                id: id_num
+            }
+        })
+    
+    useEffect(()=>{
+        }, []
+    );
+
+    if (loading) return <p>Loading2...</p>
+    if (error) return <p>Error2 :(</p>
+
+    //console.log("GetRecShows2 data",data);
+    return data;
+}
+
 //will be used as next Page to list
-function HomePage(){
-    const userID = window.localStorage.getItem("username");
-    const RecShowArray = []
-    if (userID){
-        RecShowArray = <GetUserRecs username={userID} num={4}/>
+function HomePage(props){
+    const location= useLocation(); //get username using {location.state.user}
+    const user = location.state.user;
+    console.log("user",user);
+    const recShowArray = location.state.recs;
+    console.log("recShowArray",recShowArray);
+
+    const recShowToDisplay = [];
+    var count = 0;
+    for(let i = 1; i < recShowArray.length; i++){
+        /*if(typeof(recShowArray[i]) === 'number'){
+            let current = recShowArray[i];
+            console.log("current", current);
+            let push_this = <GetShow id ={current} />
+            console.log("push_this", push_this);
+            recShowToDisplay.push(push_this);
+        }*/
+
+        
+        // calling the copied function
+        let got_data = GetShow2(recShowArray[i]);
+
+        if(got_data.key === null){ //check for this b/c responses vary
+            //console.log("THIS FAILS", got_data);
+        }
+        else{
+            //if(count <=3)//limit of 4 anime titles to be returned for homepage
+            //{
+                console.log("got_data", got_data); //sanity check
+                recShowToDisplay.push(got_data);
+                count++;
+            //}
+        }
+        //console.log("got_data", got_data);
+        //recShowToDisplay.push(got_data);
+
+        //console.log("typeof got_data",typeof got_data);
+        
     }
+    console.log("recShowToDisplay",recShowToDisplay);
+    
+    const tryShowToDisplay = [];
+    for( let x = 0; x < 4; x++) // to gather the 4 that will show on Homepage
+    {
+        tryShowToDisplay.push(recShowToDisplay[x]);
+    }
+
     const {error: errorC2, loading: loadingC2, data: dataC2} = useQuery(AIRING_NOW);
-    const {error: errorC3, loading: loadingC3, data: dataC3} = useQuery(TRY_THIS);
+    //const {error: errorC3, loading: loadingC3, data: dataC3} = useQuery(TRY_THIS);
     const {error: errorC4, loading: loadingC4, data: dataC4} = useQuery(TRENDING);
-      
-    useEffect(()=>{
-        }, []
-    );
-
-    useEffect(()=>{
-        }, []
-    );
-
-    useEffect(()=>{
-        }, []
-    );
-
-    // 
+       
 
     // airing_now array
     if (loadingC2) return <p>Loading2...</p>
     if (errorC2) return <p>Error2 :(</p>
-    const airing_arr = dataC2.Page.media;
-    const home_airing_arr = [];
+    console.log("dataC2", dataC2);
+    console.log("dataC4", dataC4);
+    const airingArr = dataC2.Page.media;
+    const homeAiringArr = [];
     for (let i = 0; i < 4; i++){
-        home_airing_arr.push(airing_arr[i]);    
+        homeAiringArr.push(airingArr[i]);    
     }
 
     //trending array
     if (loadingC4) return <p>Loading4...</p>
     if (errorC4) return <p>Error4 :(</p>
-    const trending_arr = dataC4.Page.media;
-    const home_trending_arr = [];
+    const trendingArr = dataC4.Page.media;
+    const homeTrendingArr = [];
     for (let i = 0; i < 4; i++){
-        home_trending_arr.push(trending_arr[i]);    
+        homeTrendingArr.push(trendingArr[i]);    
     }
 
     if (loadingC2) return <p>Loading2...</p>
     if (errorC2) return <p>Error2 :(</p>
-        if (loadingC3) return <p>Loading3...</p>
+    /*if (loadingC3) return <p>Loading3...</p>
 
     if (errorC3) return <p>Error3 :(</p>
 
-        // let col1_1 = {
-        //     name: (dataC2.series1.title.english==null)
-        //         ? dataC2.series1.title.romaji :
-        //         dataC2.series1.title.english,
-        //     image: dataC2.series1.coverImage.extraLarge,
-        //     genre: dataC2.series1.genres,
-        //     studio: dataC2.series1.studios.nodes[0].name,
-        //     id: dataC2.series1.id
-        // }
-    
-        
-        // let col1_2 = {
-        //     name: (dataC2.series2.title.english==null)
-        //         ? dataC2.series2.title.romaji :
-        //         dataC2.series2.title.english,
-        //     image: dataC2.series2.coverImage.extraLarge,
-        //     genre: dataC2.series2.genres,
-        //     studio: dataC2.series2.studios.nodes[0].name,
-        //     id: dataC2.series2.id
-        // }
-        
-        // let col1_3= {
-        //     name: (dataC2.series3.title.english==null)
-        //         ? dataC2.series3.title.romaji :
-        //         dataC2.series3.title.english,
-        //     image: dataC2.series3.coverImage.extraLarge,
-        //     genre: dataC2.series3.genres,
-        //     studio: dataC2.series3.studios.nodes[0].name,
-        //     id: dataC2.series3.id
-        // }
-    
-        // let col1_4 = {
-        //     name: (dataC2.series4.title.english==null)
-        //     ? dataC2.series4.title.romaji :
-        //     dataC2.series4.title.english,
-        //     image: dataC2.series4.coverImage.extraLarge,
-        //     genre: dataC2.series4.genres,
-        //     studio: dataC2.series4.studios.nodes[0].name,
-        //     id: dataC2.series4.id
-        // }
-    
         let col3_1 = {
             name: (dataC3.series1.title.english==null)
                 ? dataC3.series1.title.romaji :
@@ -139,28 +176,28 @@ function HomePage(){
             genre: dataC3.series4.genres,
             studio: dataC3.series4.studios.nodes[0].name,
             id: dataC3.series4.id
-        }
+        }*/
     return(
         <Fragment>
             <Container>
                 <TopSpace />
-                <Row id="feed_body">
-                    <Col id="feed_col">
+                <Row id="feed-body">
+                    <Col id="feed-col">
                         <Row id="subtitle">
                             <Col className="pink">
                                 <span><strong id="recommended">Recommended</strong></span>
                             </Col>
                             <Col id="grey"><span></span></Col>
                         </Row>
-                        <Row id="main_content">
-                            {RecShowArray.map((show) =>
+                        <Row id="main-content">
+                            {tryShowToDisplay.map((show) =>
                                 <HomePageShowCard 
                                     name={(show.Media.title.english==null)
                                         ? show.Media.title.romaji :
                                         show.Media.title.english}
                                     image={show.Media.coverImage.extraLarge}
                                     genre={show.Media.genres}
-                                    studio={show.Media.studios.nodes[0].name}
+                                    /*studio={show.Media.studios.nodes[0].name}*/
                                     description={show.Media.description}
                                     id={show.Media.id}
                                 />
@@ -168,10 +205,10 @@ function HomePage(){
                         </Row>
                     </Col>
                     <Col id="spacing"></Col>
-                    <Col id="feed_col">
+                    <Col id="feed-col">
                         <Row id="subtitle">
                             <Col className="purple">
-                                <Link className="trending_airing_link" id="airing_now" to={{
+                                <Link className="general-showmore-link" id="airing_now" to={{
                                     pathname:`/ShowMore/airing_now/1`,
                                     state: {
                                         query: AIRING_NOW
@@ -182,8 +219,8 @@ function HomePage(){
                             </Col>
                             <Col id="grey"><span></span></Col>
                         </Row>
-                        <Row id="main_content">
-                            {home_airing_arr.map((row)=>
+                        <Row id="main-content">
+                            {homeAiringArr.map((row)=>
                                     <HomePageShowCard
                                         name={(row.title.english==null)
                                             ? row.title.romaji :
@@ -197,14 +234,14 @@ function HomePage(){
                         </Row>
                     </Col>
                     <Col id="spacing"></Col>
-                    <Col id="feed_col">
+                    {/*<Col id="feed-col">
                         <Row id="subtitle">
                             <Col className="pink">
                                 <span id="try-this"><strong>Try This</strong></span>
                             </Col>
                             <Col id="grey"><span></span></Col>
                         </Row>
-                        <Row id="main_content">
+                        <Row id="main-content">
                         <HomePageShowCard
                                     name={col3_1.name}
                                     image={col3_1.image}
@@ -234,12 +271,12 @@ function HomePage(){
                                     id={col3_4.id}
                                     />
                         </Row>
-                    </Col>
+                                        </Col>*/}
                     <Col id="spacing"></Col>
-                    <Col id="feed_col">
+                    <Col id="feed-col">
                         <Row id="subtitle">
                             <Col className="purple">
-                                <Link className="trending_airing_link" id="trending" to={{
+                                <Link className="general-showmore-link" id="trending" to={{
                                     pathname:"/ShowMore/trending/1",
                                     state: {
                                         query: TRENDING
@@ -250,8 +287,8 @@ function HomePage(){
                             </Col>
                             <Col id="grey"><span></span></Col>
                         </Row>
-                        <Row id="main_content">
-                            {home_trending_arr.map((row)=>
+                        <Row id="main-content">
+                            {homeTrendingArr.map((row)=>
                                 <HomePageShowCard
                                     name={(row.title.english==null)
                                         ? row.title.romaji :
@@ -271,5 +308,7 @@ function HomePage(){
     );
 }
 
-//"/ShowMorePage/" + airing_arr
+
+
+//"/ShowMorePage/" + airingArr
 export default HomePage;
