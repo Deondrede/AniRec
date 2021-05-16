939839a5b8a4e9182dd11757c68b3d8f5a687711
Jason Chen <Jason.Chen53@myhunter.cuny.edu>
2021-05-15 20:06:23 -0400

    touched up show pages and bug fixes

--- test/anirec/src/components/animepages.component.js
+++ test/anirec/src/components/animepages.component.js
@@ -5,38 +5,37 @@ import { Link } from "react-router-dom";
 
 
 export function AnimeInfo(props){
-        let genreCount = props.genre.length
+    const studioArr = [];
+    props.studio.nodes.map((studios) =>(
+        studioArr.push(studios.name)
+    ))
+
+    console.log(props.format)
 
         return (
             <Fragment>
                 <Col>
-                    <Row>
-                        <p>Genres: </p>
-                        {props.genre.map((genres) =>
-                                        {
-                                            if (genreCount < 2)
-                                                return <Link key={genres} className="anime-page-genre-links" to={{
-                                                            pathname:`/GenresShowMore/${genres}/1`
-                                                        }}>{genres}</Link>
-                                            else 
-                                                genreCount--;
-                                                return  <Fragment>
-                                                            <Link key={genres} className="anime-page-genre-links" to={{
-                                                                pathname:`/GenresShowMore/${genres}/1`
-                                                                }}>{genres}
-                                                            </Link>
-                                                            <a>, </a>
-                                                        </Fragment>
-                                            }
-                                    )}
-                    </Row>
-                    <Row>
-                        <div>Summary: </div>
+                    <Row id="info-block">
+                        <div><strong>Summary:</strong> </div>
                         <div>{props.summary}</div>
                     </Row>
-                    <Row>
-                        <div>Season: {props.season} {props.seasonYear}</div>
-                        <div>Episodes: {props.episodes}</div>
+                    <br></br>
+                    <Row id="info-block">
+                        <div><strong>Season:</strong> {props.season} {props.seasonYear}</div>
+                    </Row>
+                    <br></br>
+                    <Row id="info-block">
+                        <div><strong>Episodes:</strong> {props.episodes}</div>
+                    </Row>
+                    <br></br>
+                    <Row id="info-block">
+                        <div><strong>Studios:&nbsp;</strong> </div>
+                        <div>{studioArr.join(', ')}</div>
+                    </Row>
+                    <br></br>
+                    <Row id="info-block">
+                        <div><strong>Media Format:&nbsp;</strong> </div>
+                        <div>{props.format}</div>
                     </Row>
                 </Col>
             </Fragment>
@@ -44,16 +43,15 @@ export function AnimeInfo(props){
         );
 }
 
-export function KeyStaff(props) {
-        return (
-            <Fragment>
-                <Col>
-                    <div>Studio: {props.studio}</div>
-                </Col>
-            </Fragment>
+// export function KeyStaff(props) {
+//         return (
+//             <Fragment>
+//                 <Col>
+//                 </Col>
+//             </Fragment>
             
-        );
-}
+//         );
+// }
 
 export class Rating extends Component {
     render() {
@@ -83,16 +81,16 @@ export class Recommendations extends Component {
     }
 }
 
-export class Reviews extends Component {
-    render() {
-        return (
-            <Fragment>
-                <Col>
-                <div>Review One</div>
-                <div>Review Two</div>
-                </Col>
-            </Fragment>
+// export class Reviews extends Component {
+//     render() {
+//         return (
+//             <Fragment>
+//                 <Col>
+//                 <div>Review One</div>
+//                 <div>Review Two</div>
+//                 </Col>
+//             </Fragment>
             
-        );
-    }
-}
+//         );
+//     }
+// }
--- test/anirec/src/pages/content_pages/show_page.js
+++ test/anirec/src/pages/content_pages/show_page.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment, useEffect } from "react";
 import { Container, Row, Col, Button, ButtonGroup } from "react-bootstrap";
 import TopSpace from "../../components/global_elements/TopSpacer.js";
 import { BrowserRouter as Router, Switch, Route, Link, useHistory, useParams  } from "react-router-dom";
-import { AnimeInfo, KeyStaff, Rating, Recommendations, Reviews } from "../../components/animepages.component.js";
+import { AnimeInfo, Rating, Recommendations} from "../../components/animepages.component.js";
 
 import {gql, useQuery} from '@apollo/client';
 import "./show_page.css"
@@ -33,6 +33,8 @@ export default function AnimePage(props) {
         episodes
         season
         seasonYear
+        averageScore
+        format
       }
     }
     `
@@ -60,14 +62,17 @@ export default function AnimePage(props) {
             data.series.title.english,
         image: data.series.coverImage.large,
         genre: data.series.genres,
-        studio: data.series.studios.nodes[0].name,
+        studio: data.series.studios,
         description: data.series.description,
         episodes: data.series.episodes,
         season: data.series.season,
-        seasonYear: data.series.seasonYear
+        seasonYear: data.series.seasonYear,
+        rating: data.series.averageScore,
+        format: data.series.format
     }
 
-    
+    let genreCount = animeData.genre.length
+
         return(
             <Fragment>
                 <TopSpace/>
@@ -78,60 +83,95 @@ export default function AnimePage(props) {
                             </Link>
                         </Row>
                         <Row>
-                            <p className="show-page-title">{animeData.name}</p>
+                            <Col>                            
+                            <Row>
+                                <p className="show-page-title"><strong>{animeData.name}</strong></p>
+                            </Row>
+                            <Row>
+                                <div className="show-page-genres">
+                                    {animeData.genre.map((genres) =>
+                                        {
+                                            if (genreCount < 2)
+                                                return <Link key={genres} className="show-page-genre-links" to={{
+                                                            pathname:`/GenresShowMore/${genres}/1`
+                                                        }}>{genres}</Link>
+                                            else 
+                                                genreCount--;
+                                                return  <Fragment>
+                                                            <Link key={genres} className="show-page-genre-links" to={{
+                                                                pathname:`/GenresShowMore/${genres}/1`
+                                                                }}>{genres}
+                                                            </Link>
+                                                            <a>, </a>
+                                                        </Fragment>
+                                            }
+                                    )}
+                                </div>
+                            </Row>
+                            </Col>
+
                         </Row>
                         <Router>
-                        <Row>
-                            <Col id="show-page-left-side">
-                                <Row>
-                                    <img id="" src={animeData.image}/>
-                                </Row>
-                                <Row>
-                                    <ButtonGroup vertical id="show-page-button-group">
-                                        <Link id="tab-name" to={`/AnimePage/${params.animeId}/information`}>
-                                            <Button id="side-btns">
-                                                Information
-                                            </Button>
-                                        </Link>
-                                        <Link id="tab-name" to={`/AnimePage/${params.animeId}/ratings`}>
-                                            <Button id="side-btns">
-                                                Overall Ratings
-                                            </Button>
-                                        </Link>
-
-                                        <Link id="tab-name" to={`/AnimePage/${params.animeId}/key-staff`}>
-                                            <Button id="side-btns">
-                                                Key Staff
-                                            </Button>
-                                        </Link>
-                                        <Link id="tab-name" to={`/Anime/${params.animeId}/reviews`}>
-                                            <Button id="side-btns">
-                                                Reviews
-                                            </Button>
-                                        </Link>
-                                        <Link id="tab-name" to={`/AnimePage/${params.animeId}/recommendations`}>
-                                            <Button id="side-btns">
-                                                Recommendations
-                                            </Button>
-                                        </Link>
-                                    </ButtonGroup>
-                                </Row>
-                            </Col>
-                                <Col>
-                                
+                            <Row>
+                                <Col id="show-page-left-side">
                                     <Row>
+                                        <div className="show-page-img">
+                                            <img  src={animeData.image}/>
+                                        </div>
+                                    </Row>
+                                    <Row>
+                                        <div className="rating">
+                                            <strong>
+                                                <p>Overall Rating</p>
+                                                <p>{animeData.rating} / 100</p>
+                                            </strong>
+                                        </div>
+                                        <ButtonGroup vertical id="show-page-button-group">
+                                            <Link id="tab-name" to={`/AnimePage/${params.animeId}/information`}>
+                                                <Button id="side-btns">
+                                                    Information
+                                                </Button>
+                                            </Link>
+
+
+                                            {/* <Link id="tab-name" to={`/AnimePage/${params.animeId}/key-staff`}>
+                                                <Button id="side-btns">
+                                                    Key Staff
+                                                </Button>
+                                            </Link> */}
+                                            {/* <Link id="tab-name" to={`/Anime/${params.animeId}/reviews`}>
+                                                <Button id="side-btns">
+                                                    Reviews
+                                                </Button>
+                                            </Link> */}
+                                            <Link id="tab-name" to={`/AnimePage/${params.animeId}/recommendations`}>
+                                                <Button id="side-btns">
+                                                    Recommendations
+                                                </Button>
+                                            </Link>
+                                        </ButtonGroup>
+                                    </Row>
+                                </Col>
+                                <Col id="show-page-right-side-outer">
+                                    <Row id="show-page-right-side-inner">
                                         <Switch>
                                             <Route path="/AnimePage/:animeid/ratings" component={Rating} />
-                                            <Route path="/AnimePage/:animeid/information" component={() =><AnimeInfo name = {animeData.name} genre = {animeData.genre} 
-                                                summary = {animeData.description} season = {animeData.season} seasonYear = {animeData.seasonYear}
-                                                episodes = {animeData.episodes} />} />
-                                            <Route path="/AnimePage/:animeid/key-staff" component={() =><KeyStaff studio = {animeData.studio} siteurl = {animeData.siteUrl} />} />
-                                            <Route path="/AnimePage/:animeid/reviews" component={Reviews} />
+                                            <Route path="/AnimePage/:animeid/information" component={() =>
+                                                <AnimeInfo 
+                                                    name = {animeData.name} 
+                                                    summary = {animeData.description} 
+                                                    season = {animeData.season} 
+                                                    seasonYear = {animeData.seasonYear}
+                                                    episodes = {animeData.episodes} 
+                                                    studio = {animeData.studio}  
+                                                    format={animeData.format}/>}/>
+                                            {/* <Route path="/AnimePage/:animeid/key-staff" component={() =><KeyStaff siteurl = {animeData.siteUrl} />} /> */}
+                                            {/* <Route path="/AnimePage/:animeid/reviews" component={Reviews} /> */}
                                             <Route path="/AnimePage/:animeid/recommendations" component={() => <Recommendations />} />
                                         </Switch>
                                     </Row>
                                 </Col>
-                        </Row>  
+                            </Row>  
                         </Router>
               
                     </Container>
